4a7dbddc4b8c18fd4ffa3ee9b68292c7a31639cd
cahak <haka.carolina@gmail.com>
2022-11-09 21:01:19 -0300

    subindo o arquivo base

---
+++ internal/tickets/tickets.go
@@ -0,0 +1,20 @@
+package tickets
+
+import (
+	"fmt"
+	"time"
+)
+
+type Ticket struct {
+	
+
+}
+
+// exemplo 1
+func GetTotalTickets(destination string) (int, error) {}
+
+// exemplo 2
+func GetMornings(time string) (int, error) {}
+
+// exemplo 3
+func AverageDestination(destination string, total int) (int, error) {}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	
+)
+
+// func main() {
+// 	total, err := tickets.GetTotalTickets("Brazil")
+// }
